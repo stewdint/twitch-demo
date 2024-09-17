6d627f064330b1844e5ec20c842315b52e9962c0
Lee Trout <lee@leetrout.com>
2024-09-17 16:04:23 -0400

    Add pizza_shop urls & use django_extensions

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from apps.pizza_shop import urls as pizza_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += pizza_urls.urlpatterns
